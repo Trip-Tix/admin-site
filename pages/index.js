89dcebd2afaf85dc85ab4053b9a9f4cd754e6025
Shamit Fatin <shamit187@gmail.com>
2023-08-06 04:59:59 +0600

    Clean Up

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import Image from 'next/image';
-import LoginComponent from '../components/login';
+import { Flex, Button, Heading, Center } from "@chakra-ui/react";
+import { Input as ChakraInput}  from "@chakra-ui/react" ;
 
 export default function Home() {
-  return (
-    <div>
-      <div>
-        <Image
-          src="/images/HomePageImage.jpeg"
-          alt="Image Description"
-          width={400}
-          height={300}
-        />
-      </div>
-      <div>
-        <LoginComponent />
-      </div>
-    </div>
-  );
+  return(
+    <Flex height={"100vh"} alignItems={"center"} justifyContent={"center"}>
+      <Flex direction={"column"} background={"gray.800"} p={12} rounded={6}>
+        <Heading mb={6} color={"white"} textAlign={"center"}>Log in</Heading>
+        <ChakraInput placeholder={"email"} mb={3} type="email"/>
+        <ChakraInput placeholder={"password"} mb={6} type="password"/>
+        <Button mt={4} colorScheme={"blue"}>Get Started</Button>
+      </Flex>
+    </Flex>
+  )
 }
