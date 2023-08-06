521e9a51f0664eeffa60f5d196d09a3281bb44ba
Shamit187 <shamit187@gmail.com>
2023-08-06 06:44:16 +0600

    In sleep zzz

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import { Flex, Button, Heading, Center } from "@chakra-ui/react";
 import { Input as ChakraInput}  from "@chakra-ui/react" ;
+import Image from "next/image";
 
 export default function Home() {
   return(
-    <Flex height={"100vh"} alignItems={"center"} justifyContent={"center"}>
-      <Flex direction={"column"} background={"gray.800"} p={12} rounded={6}>
-        <Heading mb={6} color={"white"} textAlign={"center"}>Log in</Heading>
+    <Flex direction={"row"} justifyContent={"space-evenly"}>
+      <Flex grow={"1"} direction={"column"} basis={"100%"}>
+        <Center>
+          <Image 
+            src="/images/HomePageImage.jpeg" 
+            alt="Picture of triptix"
+            width={1000} 
+            height={1000}
+          />
+        </Center>
+      </Flex>
+      <Flex grow={"1"} direction={"column"} background={"gray.800"} p={12} rounded={6} basis={"100%"}>
+        <Heading mb={6} textAlign={"center"}>Log in</Heading>
         <ChakraInput placeholder={"email"} mb={3} type="email"/>
         <ChakraInput placeholder={"password"} mb={6} type="password"/>
         <Button mt={4} colorScheme={"blue"}>Get Started</Button>
