4009a36eeb3446f897c0d40e3adfb48ea4749e52
Shamit187 <shamit187@gmail.com>
2023-08-07 05:20:08 +0600

    Search Filter Added

--- components/transport_main.tsx
+++ components/transport_main.tsx
@@ -1,14 +1,30 @@
 import { Flex } from "@chakra-ui/react";
 import TransportFilterBox from "./transport_filter_box";
 import TransportTable from "./transport_table";
+import { useState, useEffect } from "react";
+import { get_bus_info_url } from "@public/commonData/Api";
 
 export default function TransportMain() {
-    return (
-        <>
-            <Flex justifyContent={'space-around'} margin={'10'}>
-                    <TransportFilterBox />
-                    <TransportTable />
-            </Flex>
-        </>
-    );
+  const [transports, setTransports] = useState([]);
+
+  useEffect(() => {
+    fetch(get_bus_info_url)
+      .then((response) => response.json())
+      .then((data) => {
+        setTransports(data);
+        console.log(data); // Log the fetched data
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Flex justifyContent={"space-around"} margin={"10"}>
+        <TransportFilterBox />
+        <TransportTable transports={transports} />
+      </Flex>
+    </>
+  );
 }
