69c70215fa5b9098b87b1e07ca233e57790ef8c3
Shamit Fatin <shamit187@gmail.com>
2023-08-10 03:17:23 +0600

    Layout page commented

--- pages/admin/transport/bus/add-layout.tsx
+++ pages/admin/transport/bus/add-layout.tsx
@@ -2,17 +2,17 @@ import Layout from "@components/layout";
 import Navbar from "@components/shared/navbar";
 import TransportOptionBar from "@components/transport_option_bar";
 import { transport_optionbar_items } from "@public/commonData/TransportOptionBarData";
-import { navbar_items } from "@public/commonData/AdminNavBarData";
+import { navbar_item } from "@public/commonData/AdminNavBarData";
 import Footer from "@components/shared/footer";
 import BusLayoutAdd from "@components/bus_layout_add";
 
 export default function AddBusService() {
   return (
-    <Layout title="Add Bus Service">
-      <Navbar selected_option={navbar_items[0]} />
+    <Layout title="Add Bus Service" protectedPage={true}>
+      {/* <Navbar selected_option={navbar_item[0][0]} />
       <TransportOptionBar selectedOption={transport_optionbar_items[2]} />
       <BusLayoutAdd />
-      <Footer />
+      <Footer /> */}
     </Layout>
   );
 }
