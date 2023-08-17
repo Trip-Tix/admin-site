842fd6aba3b8279652c4125c381a843266a6dc01
Shamit Fatin <shamit187@gmail.com>
2023-08-17 14:50:39 +0600

    working

--- components/sidebar_with_header.tsx
+++ components/sidebar_with_header.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   IconButton,
@@ -21,7 +21,10 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
-} from '@chakra-ui/react'
+  useColorMode,
+  Button,
+} from "@chakra-ui/react";
+import Image from "next/image";
 import {
   FiHome,
   FiTrendingUp,
@@ -31,68 +34,90 @@ import {
   FiMenu,
   FiBell,
   FiChevronDown,
-} from 'react-icons/fi'
-import { IconType } from 'react-icons'
+} from "react-icons/fi";
+import { BsSun, BsMoonStarsFill } from "react-icons/bs";
+import { IconType } from "react-icons";
+import TripTixLogoLight from "@public/TripTixLogo.svg";
+import TripTixLogoDark from "@public/TripTixLogoBlack.svg";
 
 interface LinkItemProps {
-  name: string
-  icon: IconType
+  name: string;
+  icon: IconType;
 }
 
 interface NavItemProps extends FlexProps {
-  icon: IconType
-  children: React.ReactNode
+  icon: IconType;
+  children: React.ReactNode;
 }
 
 interface MobileProps extends FlexProps {
-  onOpen: () => void
+  onOpen: () => void;
 }
 
 interface SidebarProps extends BoxProps {
-  onClose: () => void
+  onClose: () => void;
 }
 
 const LinkItems: Array<LinkItemProps> = [
-  { name: 'Home', icon: FiHome },
-  { name: 'Trending', icon: FiTrendingUp },
-  { name: 'Explore', icon: FiCompass },
-  { name: 'Favourites', icon: FiStar },
-  { name: 'Settings', icon: FiSettings },
-]
+  { name: "Home", icon: FiHome },
+  { name: "Trending", icon: FiTrendingUp },
+  { name: "Explore", icon: FiCompass },
+  { name: "Favourites", icon: FiStar },
+  { name: "Settings", icon: FiSettings },
+];
 
 const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box
       transition="3s ease"
-      bg={useColorModeValue('white', 'gray.900')}
+      bg={useColorModeValue("white", "gray.900")}
       borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
+      borderRightColor={useColorModeValue("gray.200", "gray.700")}
+      w={{ base: "full", md: 60 }}
       pos="fixed"
       h="full"
-      {...rest}>
+      {...rest}
+    >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
-        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+        <HStack spacing={8} alignItems="center">
+          <Image
+            src={colorMode=="light"? TripTixLogoDark: TripTixLogoLight}
+            alt="Triptix Logo"
+            priority
+            placeholder="empty"
+            height={30}
+          />
+          <Text fontSize="2xl" fontWeight="bold">
+            Triptix
+          </Text>
+        </HStack>
+
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
       {LinkItems.map((link) => (
         <NavItem key={link.name} icon={link.icon}>
           {link.name}
         </NavItem>
       ))}
+      <Flex
+        h="20"
+        alignItems="center"
+        mx="8"
+        justifyContent="space-between"
+      ></Flex>
     </Box>
-  )
-}
+  );
+};
 
 const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
   return (
     <Box
       as="a"
       href="#"
-      style={{ textDecoration: 'none' }}
-      _focus={{ boxShadow: 'none' }}>
+      style={{ textDecoration: "none" }}
+      _focus={{ boxShadow: "none" }}
+    >
       <Flex
         align="center"
         p="4"
@@ -101,16 +126,17 @@ const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
         role="group"
         cursor="pointer"
         _hover={{
-          bg: 'cyan.400',
-          color: 'white',
+          bg: "cyan.400",
+          color: "white",
         }}
-        {...rest}>
+        {...rest}
+      >
         {icon && (
           <Icon
             mr="4"
             fontSize="16"
             _groupHover={{
-              color: 'white',
+              color: "white",
             }}
             as={icon}
           />
@@ -118,67 +144,87 @@ const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
         {children}
       </Flex>
     </Box>
-  )
-}
+  );
+};
 
 const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Flex
       ml={{ base: 0, md: 60 }}
       px={{ base: 4, md: 4 }}
       height="20"
       alignItems="center"
-      bg={useColorModeValue('white', 'gray.900')}
+      bg={useColorModeValue("white", "gray.900")}
       borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent={{ base: 'space-between', md: 'flex-end' }}
-      {...rest}>
+      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      justifyContent={{ base: "space-between", md: "flex-end" }}
+      {...rest}
+    >
       <IconButton
-        display={{ base: 'flex', md: 'none' }}
+        display={{ base: "flex", md: "none" }}
         onClick={onOpen}
         variant="outline"
         aria-label="open menu"
         icon={<FiMenu />}
       />
 
-      <Text
-        display={{ base: 'flex', md: 'none' }}
-        fontSize="2xl"
-        fontFamily="monospace"
-        fontWeight="bold">
-        Logo
-      </Text>
+      <HStack
+        spacing={{ base: "5", md: "6" }}
+        display={{ base: "flex", md: "none" }}
+      >
+        <Image
+          src={colorMode=="light"? TripTixLogoDark: TripTixLogoLight}
+          alt="Triptix Logo"
+          priority
+          placeholder="empty"
+          height={30}
+        />
+        <Text fontSize="2xl" fontWeight="bold">
+          Triptix
+        </Text>
+      </HStack>
 
-      <HStack spacing={{ base: '0', md: '6' }}>
-        <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
-        <Flex alignItems={'center'}>
+      <HStack spacing={{ base: "5", md: "6" }}>
+        <Button
+          aria-label="Toggle Color Mode"
+          onClick={toggleColorMode}
+          _focus={{ boxShadow: "none" }}
+          w="fit-content"
+        >
+          {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun />}
+        </Button>
+        <Flex alignItems={"center"}>
           <Menu>
-            <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
+            <MenuButton
+              py={2}
+              transition="all 0.3s"
+              _focus={{ boxShadow: "none" }}
+            >
               <HStack>
                 <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
+                  size={"sm"}
                 />
                 <VStack
-                  display={{ base: 'none', md: 'flex' }}
+                  display={{ base: "none", md: "flex" }}
                   alignItems="flex-start"
                   spacing="1px"
-                  ml="2">
+                  ml="2"
+                >
                   <Text fontSize="sm">Justina Clark</Text>
                   <Text fontSize="xs" color="gray.600">
                     Admin
                   </Text>
                 </VStack>
-                <Box display={{ base: 'none', md: 'flex' }}>
+                <Box display={{ base: "none", md: "flex" }}>
                   <FiChevronDown />
                 </Box>
               </HStack>
             </MenuButton>
             <MenuList
-              bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}>
+              bg={useColorModeValue("white", "gray.900")}
+              borderColor={useColorModeValue("gray.200", "gray.700")}
+            >
               <MenuItem>Profile</MenuItem>
               <MenuItem>Settings</MenuItem>
               <MenuItem>Billing</MenuItem>
@@ -189,26 +235,30 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
         </Flex>
       </HStack>
     </Flex>
-  )
-}
+  );
+};
 
 interface SidebarWithHeaderProps {
-    children: React.ReactNode;
-  }
+  children: React.ReactNode;
+}
 
-const SidebarWithHeader = ({children} : SidebarWithHeaderProps) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+const SidebarWithHeader = ({ children }: SidebarWithHeaderProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
       <Drawer
         isOpen={isOpen}
         placement="left"
         onClose={onClose}
         returnFocusOnClose={false}
         onOverlayClick={onClose}
-        size="full">
+        size="full"
+      >
         <DrawerContent>
           <SidebarContent onClose={onClose} />
         </DrawerContent>
@@ -218,7 +268,7 @@ const SidebarWithHeader = ({children} : SidebarWithHeaderProps) => {
         {children}
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default SidebarWithHeader
+export default SidebarWithHeader;
---
+++ pages/list.tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/layout";
+import SidebarWithHeader from "@components/sidebar_with_header";
+
+export default function Main() {
+    return (
+        <Layout title="List">
+            <SidebarWithHeader>
+                <p>List</p>    
+            </SidebarWithHeader>
+        </Layout>
+    );
+}
