a393457617fb319993a98ba513b700004d10f38a
fahmid111 <1805036@ugrad.cse.buet.ac.bd>
2023-08-20 22:24:35 +0600

    List Train Update 01

--- pages/list/train.tsx
+++ pages/list/train.tsx
@@ -1,35 +1,27 @@
+import { HStack, VStack } from "@chakra-ui/react";
+import Layout from "@components/layout";
+import SidebarWithHeader from "@components/sidebar_with_header";
+// import Stat from "@components/list_train/stat";
+// import MainData from "@components/list_train/main_data";
+import TransportSelect from "@components/transport_select";
 import {
-    Box,
-    HStack,
-    VStack,
-    Card,
-    CardBody,
-    Text,
-    Stat,
-  } from "@chakra-ui/react";
-  import Layout from "@components/layout";
-  import SidebarWithHeader from "@components/sidebar_with_header";
-  import StatCard from "@components/list_bus/stat_card";
-  import List from "@components/list_bus/list";
-  import Details from "@components/list_bus/details";
-  import TransportSelect from "@components/transport_select";
-  import {
-    NavigationOption,
-    TransportType,
-  } from "@public/common/navigation_option";
-  
-  export default function Main() {
-    return (
-      <Layout title="List Train" isProtected={true}>
-        <SidebarWithHeader navItem={NavigationOption.List}>
-          <VStack spacing="4" align="stretch">
-            <TransportSelect
-              transport={TransportType.Train}
-              navigation={NavigationOption.List}
-            />
-          </VStack>
-        </SidebarWithHeader>
-      </Layout>
-    );
-  }
-  
+  NavigationOption,
+  TransportType,
+} from "@public/common/navigation_option";
+
+export default function Main() {
+  return (
+    <Layout title="List Bus" isProtected={true}>
+      <SidebarWithHeader navItem={NavigationOption.List}>
+        <VStack spacing="4" align="stretch">
+          <TransportSelect
+            transport={TransportType.Train}
+            navigation={NavigationOption.List}
+          />
+          {/* <Stat /> */}
+          {/* <MainData /> */}
+        </VStack>
+      </SidebarWithHeader>
+    </Layout>
+  );
+}
