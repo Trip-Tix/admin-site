90007708d3625751b7127e49727b6a115b18bdd2
fahmid111 <1805036@ugrad.cse.buet.ac.bd>
2023-08-21 03:52:20 +0600

    Train Add Update 01

--- pages/add/train.tsx
+++ pages/add/train.tsx
@@ -1,18 +1,35 @@
-import { Box, HStack, VStack, Card, CardBody, Text, Stat } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import Layout from "@components/layout";
 import SidebarWithHeader from "@components/sidebar_with_header";
-import StatCard from "@components/stat_card";
-import List from "@components/list_bus/list";
-import Details from "@components/list_bus/details";
 import TransportSelect from "@components/transport_select";
-import { NavigationOption, TransportType } from "@public/common/navigation_option";
+import {
+  NavigationOption,
+  TransportType,
+} from "@public/common/navigation_option";
+import { useState, useEffect } from "react";
+
 
 export default function Main() {
+  const [trainName, setTrainName] = useState("");
+  const [coaches, setCoaches] = useState<CoachInfo[]>([]);
+  const [submit, setSubmit] = useState(false);
+  const [check, setCheck] = useState(false);
+  const [coachKeys, setCoachKeys] = useState<number[]>([]);
+
+  useEffect (() => {
+    if (check && trainName && coachKeys.length > 0) {
+      setSubmit(true);
+    }
+  }, [check]);
+
   return (
     <Layout title="Add Train" isProtected={true}>
       <SidebarWithHeader navItem={NavigationOption.Add}>
         <VStack spacing="4" align="stretch">
-          <TransportSelect transport={TransportType.Train} navigation={NavigationOption.Add} />
+            <TransportSelect
+              transport={TransportType.Train}
+              navigation={NavigationOption.Add}
+            />
         </VStack>
       </SidebarWithHeader>
     </Layout>
