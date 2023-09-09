86a930c211f6f27723618b8460cbf2e4784c3509
fahmid111 <1805036@ugrad.cse.buet.ac.bd>
2023-09-09 19:36:06 +0600

    train stat update

---
+++ components/list_train/stat.tsx
@@ -0,0 +1,47 @@
+import StatCard from "@components/stat_card";
+import { HStack } from "@chakra-ui/react";
+import {
+  getAvailableTrain,
+  getCustomerRatingTrain,
+  getTotalRouteTrain,
+  getTotalOnRoutesTrain,
+} from "@public/common/api";
+
+
+const data = [
+  {
+    title: "Total Train",
+    apiLink: getAvailableTrain,
+    cardImage: "/images/train_available.svg"
+  },
+  {
+    title: "Total Route",
+    apiLink: getTotalRouteTrain,
+    cardImage: "/images/route.svg"
+  },
+  {
+    title: "Total On Route",
+    apiLink: getTotalOnRoutesTrain,
+    cardImage: "/images/route_total.svg"
+  },
+  {
+    title: "Customer Rating",
+    apiLink: getCustomerRatingTrain,
+    cardImage: "/images/rating.svg"
+  },
+];
+
+export default function Stat() {
+  return (
+    <HStack
+      spacing="4"
+      align="stretch"
+      justify="space-between"
+      display={{ base: "none", md: "flex" }}
+    >
+        {data.map((item) => (
+            <StatCard key={item.title} cardTitle={item.title} apiLink={item.apiLink} cardImage={item.cardImage}/>
+        ))}
+    </HStack>
+  );
+}
